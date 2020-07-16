13bb733dd3913366838268dac5eae6305b00bde6
Germany2k9 <germanyp20l@gmail.com>
2020-07-16 02:42:24 +0200

    primer commit

---
+++ login.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta http-equiv="x-ua-compatible" content="ie-edge">
+	<link rel="stylesheet"  type="text/css" href="css/bootstrap.css">
+  	<link rel="stylesheet"  type="text/css" href="css/style.css">
+  	<link rel=”shortcut icon” type=”image/png” href=”img/favicon.png”/>
+   	<link rel="stylesheet"  type="text/css" href="css/alertify.min.css">
+	<!-- <link rel="stylesheet"  type="text/css" href="css/bootstrap.min.css"> -->
+	<title>Document</title>
+</head>
+<body> 
+<h2>login</h2>
+<div class="container">
+	<form action="" action="acceso.php" method="POST">
+		<div class="form-group">
+		<label for="">Usuario:</label>
+		<input type="text" name="usuario" class="form-control" >
+		</div>
+		<div class="form-group">
+		<label for="">Contrasenia:</label>
+		<input type="password" name="password" class="form-control" >
+		</div>
+		<div class="form-group">
+		<input type="submit" class=" btn btn-success" >
+		</div>
+	</form>
+</div>
+
+  
+
+  
+
+	 <script src="js/jquery-3.4.1.min.js"></script>
+	 <script src="js/popper.min.js"></script>
+	 <script src="js/bootstrap.min.js"></script>
+  	 <script src="js/alertify.min.js"></script>
+   	 <script src="alertify/alertify.min.js"></script>
+   	 <script src="moment/moment-with-locales.js"></script>
+	 <script>
+	 	$( document ).ready(function() {
+ 	 	console.log("Are /you READY!!");
+      
+   		 });
+ 
+	</script>
+  <script src="https://kit.fontawesome.com/a03bedb3c1.js" crossorigin="anonymous"></script>
+</body>
+</html>
